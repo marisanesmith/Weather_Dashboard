c093529703d07e187d72d52e4edfa50853653c0f
Marisa Nesmith <marisanesmith@Marisas-iMac.local>
2021-01-23 20:22:00 -0500

    updated JS file

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,33 @@
-var APIKey = "95c61d37a98e509e8766816d7fd6ec65"
+//Global Variables
+
+const searchCity = $("#search-city");
+const searchButton = $("#search-button");
+const clearHistory = $("#clear-history");
+const currentWeather = $("#current-weather");
+const currentCity = $("#current-city");
+const temperature = $("#temperature");
+const humidity = $("#humidity");
+const windSpeed = $("#wind-speed");
+const UVIndex = $("#uv-index");
+
+var city = "";
+
+//set up the API key
+var APIKey = "95c61d37a98e509e8766816d7fd6ec65";
+
+//Set up the AJAX call
+var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+console.log(queryURL)
+
+
+$.ajax ({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+
+});
+
+console.log("URL: " + queryURL)
+
+// convert temp to F
+var tempF = (response.main.temp - 273.15) * 1.80 + 32;
